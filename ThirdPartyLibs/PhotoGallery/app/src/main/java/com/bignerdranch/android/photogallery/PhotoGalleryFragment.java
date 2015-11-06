aa9f25b3ae7a469bece96dde278094ab6edff562
Bolot Kerimbaev <bolot@users.noreply.github.com>
2015-11-06 11:14:07 -0800

    Picasso in PhotoGallery

--- ThirdPartyLibs/PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ ThirdPartyLibs/PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,12 +1,8 @@
 package com.bignerdranch.android.photogallery;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
@@ -19,11 +15,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
-import butterknife.BindDrawable;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -32,7 +29,6 @@ public class PhotoGalleryFragment extends VisibleFragment {
 
     @Bind(R.id.fragment_photo_gallery_recycler_view) protected RecyclerView mPhotoRecyclerView;
     private List<GalleryItem> mItems = new ArrayList<>();
-    private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -46,19 +42,6 @@ public class PhotoGalleryFragment extends VisibleFragment {
 
         updateItems();
 
-        Handler responseHandler = new Handler();
-        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
-        mThumbnailDownloader.setThumbnailDownloadListener(
-            new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
-                @Override
-                public void onThumbnailDownloaded(PhotoHolder photoHolder, Bitmap bitmap) {
-                    Drawable drawable = new BitmapDrawable(getResources(), bitmap);
-                    photoHolder.bindDrawable(drawable);
-                }
-            }
-        );
-        mThumbnailDownloader.start();
-        mThumbnailDownloader.getLooper();
         Log.i(TAG, "Background thread started");
     }
 
@@ -77,14 +60,12 @@ public class PhotoGalleryFragment extends VisibleFragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        mThumbnailDownloader.clearQueue();
         ButterKnife.unbind(this);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mThumbnailDownloader.quit();
         Log.i(TAG, "Background thread destroyed");
     }
 
@@ -165,12 +146,12 @@ public class PhotoGalleryFragment extends VisibleFragment {
             ButterKnife.bind(this, itemView);
         }
 
-        public void bindDrawable(Drawable drawable) {
-            mItemImageView.setImageDrawable(drawable);
-        }
-
         public void bindGalleryItem(GalleryItem galleryItem) {
             mGalleryItem = galleryItem;
+            Picasso.with(getContext())
+                    .load(galleryItem.getUrl())
+                    .placeholder(R.drawable.bill_up_close)
+                    .into(mItemImageView);
         }
 
         @OnClick(R.id.fragment_photo_gallery_image_view)
@@ -184,7 +165,6 @@ public class PhotoGalleryFragment extends VisibleFragment {
     protected class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
 
         private List<GalleryItem> mGalleryItems;
-        @BindDrawable(R.drawable.bill_up_close) protected Drawable mPlaceholder;
 
         public PhotoAdapter(List<GalleryItem> galleryItems) {
             mGalleryItems = galleryItems;
@@ -202,8 +182,6 @@ public class PhotoGalleryFragment extends VisibleFragment {
         public void onBindViewHolder(PhotoHolder photoHolder, int position) {
             GalleryItem galleryItem = mGalleryItems.get(position);
             photoHolder.bindGalleryItem(galleryItem);
-            photoHolder.bindDrawable(mPlaceholder);
-            mThumbnailDownloader.queueThumbnail(photoHolder, galleryItem.getUrl());
         }
 
         @Override
